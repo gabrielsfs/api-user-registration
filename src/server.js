5c030cea1a166945279731571a5da343d86685e7
Gabriel <gabriel.finoto@nextpage.com.br>
2020-01-11 21:26:14 -0300

    criando conexao com banco de dados

--- src/server.js
+++ src/server.js
@@ -1,24 +1,17 @@
-import express from 'express'
-import dbConfig from './db/config'
-import user from './routes/user'
+import express from 'express';
+import dbConfig from './db/config';
+import mysql from 'mysql'
+import user from './routes/user';
 
 const PORT = 2000;
+
+const pool = mysql.createPool(dbConfig)
+
 const app = express();
 
-
-//verificar se existe um banco de dados
-//verificar se existe uma tabela
-
+app.pool = pool;
 
 app.use('user', user)
-
-
-
-
-
-
-
-
 
 app.listen(PORT, () => {
     console.log(`Started on port ${PORT}`)
